bbfabac4fdf6af310cbb6aeb63d783c928aa478b
Siratee K <sirateek@gmail.com>
2022-10-22 22:53:39 +0700

    fix: Remove swagger

--- httpserver/httpserver.go
+++ httpserver/httpserver.go
@@ -2,15 +2,10 @@ package httpserver
 
 import (
 	"github.com/gin-gonic/gin"
-
-	ginSwagger "github.com/swaggo/gin-swagger"
-
-	swaggerfiles "github.com/swaggo/files"
 )
 
 func NewHttpServer() *gin.Engine {
 	router := gin.Default()
 	router.GET("/healthcheck", healthCheckHandler)
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	return router
 }
