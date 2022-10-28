876ba9b6c8716f1e674d8d00052e609e13d30bb1
Siratee K <sirateek@gmail.com>
2022-10-28 20:36:36 +0700

    refactor: Fix the logger and return error if it happens

--- database/databasemysql.go
+++ database/databasemysql.go
@@ -14,33 +14,32 @@ type Config struct {
 	DatabaseName string
 }
 
-func NewDbConnection(config Config) *sql.DB {
+func newLogrusEntry(config Config) *logrus.Entry {
+	return logrus.WithFields(logrus.Fields{
+		"Database": "MYSQL",
+		"Hostname": config.Hostname,
+	})
+}
+
+func NewDbConnection(config Config) (*sql.DB, error) {
+	logger := newLogrusEntry(config)
 
 	db, err := sql.Open(
 		"mysql",
 		config.Username+":"+config.Password+"@tcp("+config.Hostname+")/"+config.DatabaseName,
 	)
 	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"Database": "MYSQL",
-			"Hostname": config.Hostname,
-		}).Error(err)
+		logger.Error(err)
+		return nil, err
 	}
 
 	// check the connection
 	err = db.Ping()
 
 	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"Database": "MYSQL",
-			"Hostname": config.Hostname,
-		}).Error(err)
-	} else {
-		logrus.WithFields(logrus.Fields{
-			"Database": "MYSQL",
-			"Hostname": config.Hostname,
-		}).Info("Connected to Database")
+		logger.Error(err)
+		return nil, err
 	}
-
-	return db
+	logger.Info("Connected to Database")
+	return db, nil
 }
